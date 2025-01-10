c0cd1f4431f04b907bc75c7caeb7a2f90f439a6c
= <=>
2025-01-10 07:10:55 +0100

    Added delta

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill(000)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time()/1000
     
 
 if __name__ == "__main__":
